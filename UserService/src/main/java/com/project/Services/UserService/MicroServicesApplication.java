a0d4a4529adcd408c4abc1601ae06780f3c26684
Andrey Chizhov <92399109+AndreyChizhovD@users.noreply.github.com>
2023-06-22 14:57:11 +0300

    Add files via upload
    added user service

---
+++ UserService/src/main/java/com/project/Services/UserService/MicroServicesApplication.java
@@ -0,0 +1,16 @@
+package com.project.Services.UserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//127.0.0.1 8080
+@SpringBootApplication
+public class MicroServicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicesApplication.class, args);
+    }
+}
+
+
+
+
